3b3865890680f9033cedbd1b7cf57b92b746920d
Josimar Souza <contact.josimarsouza@gmail.com>
2023-06-01 16:29:46 -0300

    feat: Added more methods to CountriesAPI class

--- src/domain/countries/index.js
+++ src/domain/countries/index.js
@@ -21,6 +21,56 @@ class CountriesAPI {
       return new ErrorCreator(e, 'Não foi possível encontrar os países, por favor, tente mais tarde!');
     }
   }
+
+  async getCountryByName(name) {
+    try {
+      const result = await this.api.get(`/name/${name}`);
+
+      return result;
+    } catch (e) {
+      return new ErrorCreator(e, `Não foi possível encontrar o país "${name}"`);
+    }
+  }
+
+  async getCountryByCurrency(currency) {
+    try {
+      const result = await this.api.get(`/currency/${currency}`);
+
+      return result;
+    } catch (e) {
+      return new ErrorCreator(e, `Não foi possível encontrar o país pela moeda "${currency}"`);
+    }
+  }
+
+  async getCountryByCapitalCity(capital) {
+    try {
+      const result = await this.api.get(`/capital/${capital}`);
+
+      return result;
+    } catch (e) {
+      return new ErrorCreator(e, `Não foi possível encontrar o país pela moeda "${capital}"`);
+    }
+  }
+
+  async getCountriesByRegion(region) {
+    try {
+      const result = await this.api.get(`/region/${region}`);
+
+      return result;
+    } catch (e) {
+      return new ErrorCreator(e, `Não foi possível encontrar países na região "${region}"`);
+    }
+  }
+
+  async getCountriesByLanguage(language) {
+    try {
+      const result = await this.api.get(`/lang/${language}`);
+
+      return result;
+    } catch (e) {
+      return new ErrorCreator(e, `Não foi possível encontrar países com o idioma "${language}"`);
+    }
+  }
 }
 
 export default CountriesAPI;
