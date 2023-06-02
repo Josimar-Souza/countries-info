29a8cce491e4bfaebbf9f4a189f0f0334be73a6b
Josimar Souza <contact.josimarsouza@gmail.com>
2023-06-02 14:58:42 -0300

    feat: Light and dark themes created

---
+++ src/globalStyles/themes/index.js
@@ -0,0 +1,26 @@
+const lightTheme = {
+  background: '#eff1ed',
+  colors: {
+    cards: '#80a4ed',
+    actions: '#433a3f',
+    text: 'black',
+    reserveColor: '#dd7596',
+  },
+};
+
+const darkTheme = {
+  background: '#001524',
+  colors: {
+    cards: '#15616d',
+    actions: '#704c5e',
+    text: 'white',
+    reserveColor: '#9e829c',
+  },
+};
+
+const themes = {
+  lightTheme,
+  darkTheme,
+};
+
+export default themes;
