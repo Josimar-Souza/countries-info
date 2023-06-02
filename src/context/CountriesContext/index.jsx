8a32c93f9331e09214976e184f5565f88fd264ad
Josimar Souza <contact.josimarsouza@gmail.com>
2023-06-02 14:30:08 -0300

    feat: Added countries search to context and sendNotification helper

--- src/context/CountriesContext/index.jsx
+++ src/context/CountriesContext/index.jsx
@@ -1,12 +1,38 @@
-import React, { createContext, useState, useMemo } from 'react';
+import React, {
+  createContext,
+  useState,
+  useMemo,
+  useEffect,
+} from 'react';
 import PropTypes from 'prop-types';
+import CountriesAPI from '../../domain/countries';
+import ErrorCreator from '../../helpers/ErrorCreator';
+import sendNotification from '../../helpers/sendNotification';
 
 export const countriesContext = createContext();
+const countriesAPI = new CountriesAPI();
 
 function CountriesContext({ children }) {
   const [countries, setCountries] = useState([]);
 
-  const contextValue = useMemo(() => ({ countries, setCountries }), []);
+  const getAllCountries = async () => {
+    const result = await countriesAPI.getAllCountries();
+
+    if (result instanceof ErrorCreator) {
+      sendNotification(result.customMessage, 'error');
+    } else {
+      setCountries(result);
+    }
+  };
+
+  useEffect(() => {
+    getAllCountries();
+  }, []);
+
+  const contextValue = useMemo(() => ({
+    countries,
+    setCountries,
+  }), [countries]);
 
   return (
     <countriesContext.Provider value={contextValue}>
