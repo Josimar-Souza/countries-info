c8771de1d0a7077d7ccc7e931e837b269dffa990
Josimar Souza <contact.josimarsouza@gmail.com>
2023-06-02 17:53:56 -0300

    feat: Added search form items

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,9 +1,55 @@
 import React from 'react';
-import SearchContainer from './styles';
+
+import {
+  Form,
+  Input,
+  Button,
+} from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+import {
+  SearchContainer,
+  TermSearchContainer,
+  TypeSearchContainer,
+  FormItemsContainer,
+  TypeSelect,
+  TypeSelectOption,
+} from './styles';
 
 function Search() {
+  const onSearchFinished = (values) => {
+    console.log(values);
+  };
+
   return (
-    <SearchContainer />
+    <SearchContainer>
+      <Form layout="inline" name="countries-search" onFinish={onSearchFinished}>
+        <FormItemsContainer>
+          <SearchOutlined style={{ fontSize: '28px' }} />
+          <TermSearchContainer>
+            <Form.Item
+              name="term"
+            >
+              <Input placeholder="Type the term to search" />
+            </Form.Item>
+          </TermSearchContainer>
+          <TypeSearchContainer>
+            <Form.Item name="type" initialValue="name">
+              <TypeSelect defaultValue="name">
+                <TypeSelectOption value="name">Name</TypeSelectOption>
+                <TypeSelectOption value="currency">Currency</TypeSelectOption>
+                <TypeSelectOption value="capital">Capital city</TypeSelectOption>
+                <TypeSelectOption value="region">Region</TypeSelectOption>
+                <TypeSelectOption value="lang">Language</TypeSelectOption>
+              </TypeSelect>
+            </Form.Item>
+          </TypeSearchContainer>
+          <Button htmlType="submit">
+            <SearchOutlined />
+            Search
+          </Button>
+        </FormItemsContainer>
+      </Form>
+    </SearchContainer>
   );
 }
 
--- src/components/Search/styles.js
+++ src/components/Search/styles.js
@@ -1,10 +1,70 @@
 import styled from 'styled-components';
 
-const SearchContainer = styled.div`
-  border: 1px solid red;
+export const SearchContainer = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
   margin-top: 5rem;
   min-height: 100px;
   width: 100%;
+
+  form {
+    width: 100%;
+  }
 `;
 
-export default SearchContainer;
+export const FormItemsContainer = styled.div`
+  align-items: center;
+  color: ${({ theme }) => theme.colors.searchColor};
+  display: flex;
+  justify-content: center;
+  width: 100%;
+
+  button {
+    background-color: ${({ theme }) => theme.colors.searchBackgroundColor};
+    border: none;
+    color: ${({ theme }) => theme.colors.searchColor};
+    font-size: 22px;
+    font-weight: 700;
+    min-height: 50px;
+    width: 10%;
+  }
+`;
+
+export const TermSearchContainer = styled.div`
+  margin-right: 15px;
+  width: 50%;
+
+  input {
+    background-color: ${({ theme }) => theme.colors.searchBackgroundColor};
+    border: none;
+    color: ${({ theme }) => theme.colors.searchColor};
+    font-size: 22px;
+    font-weight: 700;
+    margin-left: 10px;
+    min-height: 50px;
+    width: 100%;
+  }
+`;
+
+export const TypeSearchContainer = styled.div`
+  margin-right: 15px;
+  width: 10%;
+`;
+
+export const TypeSelect = styled.select`
+  background-color: ${({ theme }) => theme.colors.searchBackgroundColor};
+  border: none;
+  border-radius: 10px;
+  color: ${({ theme }) => theme.colors.searchColor};
+  font-size: 22px;
+  font-weight: 700;
+  min-height: 50px;
+  padding: 10px;
+  width: 100%;
+`;
+
+export const TypeSelectOption = styled.option`
+  font-size: 22px;
+  font-weight: 700;
+`;
