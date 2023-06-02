2693b6c3a597b22a286723c69c4c465645596d3c
Josimar Souza <contact.josimarsouza@gmail.com>
2023-06-02 15:04:22 -0300

    feat: Added changeTheme method to ThemeContext

--- src/context/ThemeContext/index.jsx
+++ src/context/ThemeContext/index.jsx
@@ -1,20 +1,32 @@
 import React, { createContext, useMemo, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import PropType from 'prop-types';
+import themes from '../../globalStyles/themes';
 
 export const themeContext = createContext();
 
 function ThemeContext({ children }) {
-  const [currTheme, setCurrTheme] = useState({});
+  const [currTheme, setCurrTheme] = useState({ theme: themes.lightTheme, darkMode: false });
+
+  const changeTheme = () => {
+    const { lightTheme, darkTheme } = themes;
+
+    if (currTheme.darkMode) {
+      setCurrTheme({ theme: lightTheme, darkMode: false });
+    } else {
+      setCurrTheme({ theme: darkTheme, darkMode: true });
+    }
+  };
 
   const contextValue = useMemo(() => ({
     currTheme,
     setCurrTheme,
+    changeTheme,
   }), [currTheme]);
 
   return (
     <themeContext.Provider value={contextValue}>
-      <ThemeProvider theme={currTheme}>
+      <ThemeProvider theme={currTheme.theme}>
         { children }
       </ThemeProvider>
     </themeContext.Provider>
